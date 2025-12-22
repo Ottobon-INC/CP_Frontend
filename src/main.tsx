12f73a57d355b2e557abf2faf0028ade97229c53
Pramod <pramodlucky2525@gmail.com>
2025-12-22 10:58:32 +0530

    testing commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,5 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 //this is a sample line
+//another one
 createRoot(document.getElementById("root")!).render(<App />);
