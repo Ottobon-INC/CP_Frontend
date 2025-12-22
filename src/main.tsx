953328cd05f9ac89f8da3f8524b3fc2263c418e4
Pramod <pramodlucky2525@gmail.com>
2025-12-22 10:49:11 +0530

    deleted examnple

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-
+//this is a sample line
 createRoot(document.getElementById("root")!).render(<App />);
